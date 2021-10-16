d8c6c32aa723d470e4d993aa116b0ff4a31725e4
angelolocritani <67960642+angelolocritani@users.noreply.github.com>
2021-10-16 17:45:00 +0200

    Update NightOwl_Patches.cs
    Patching Pawn.SetFaction covers both recruiting and buying a night owl pawn

--- Source/TinyTweaks/HarmonyPatches/NightOwl_Patches.cs
+++ Source/TinyTweaks/HarmonyPatches/NightOwl_Patches.cs
@@ -63,18 +63,19 @@ namespace TinyTweaks
             }
         }
 
-        [HarmonyPatch(typeof(InteractionWorker_RecruitAttempt), "DoRecruit", new System.Type[] { typeof(Pawn), typeof(Pawn), typeof(string), typeof(string), typeof(bool), typeof(bool) },
-                                                                             new ArgumentType[] { ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Out, ArgumentType.Out, ArgumentType.Normal, ArgumentType.Normal })]
-        public static class InteractionWorker_RecruitAttempt_DoRecruit
+
+
+        [HarmonyPatch(typeof(Pawn), "SetFaction", new System.Type[] { typeof(Faction), typeof(Pawn) })]
+        public static class Pawn_SetFaction
         {
-            public static void Postfix(Pawn recruitee)
+            public static void Postfix(Pawn __instance)
             {
                 if (!TinyTweaksSettings.autoOwl)
                 {
                     return;
                 }
 
-                if (recruitee is not { } pawn)
+                if (!(__instance is Pawn pawn))
                 {
                     return;
                 }
